3803c97a8065b5a6e54bf3ad260fb95776c72845
Chitransh Mankar <chitranshm13@gmail.com>
2026-03-30 19:49:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.set_page_config(page_title="AI Resume Screener | Mankar Official")
 # ==========================================
 load_dotenv()
 # Direct Key 
-genai.configure(api_key="enter_your_real_key")
+genai.configure(api_key="GOOGLE_API_KEY")
 
 
 # Keep vector database always accessible
